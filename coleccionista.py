68448cbfab715a80988c97788d5a04e9453819dd
jhojannsossa <cbbainboxbol@gmail.com>
2026-09-24 12:19:21 +0200

    printWelcomeAndCatalog

---
+++ coleccionista.py
@@ -0,0 +1,6 @@
+print("======================================================")
+print("  Bienvenido al Catálogo de Piezas Coleccionables  ")
+print("======================================================")
+
+catalog = []
+categorias_set = set()
